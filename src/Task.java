d240056164a4418e27c4f0e8d9440d0eda88bde5
Jayden <jdecambre10@gmail.com>
2024-09-24 17:57:22 -0400

    second commit

--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,8 @@
 public class Task {
 
     public static void main (String[] args){
-        System.out.println("hi");
+        System.out.println("hi!");
+        System.out.println("hello again!");
 
     }
 }
